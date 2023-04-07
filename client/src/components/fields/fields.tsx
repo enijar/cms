0592d0a5ea51128d4d29c60dc30e8de885fd0874
Enijar <TheEnijar@gmail.com>
2023-04-07 16:49:11 +0100

    chore: remove debug log

--- client/src/components/fields/fields.tsx
+++ client/src/components/fields/fields.tsx
@@ -22,8 +22,6 @@ export default function Fields({ schema }: Props) {
     return schemaToFields(schema);
   }, [schema]);
 
-  // console.log(fields.find((f) => f.type === "list"));
-
   return (
     <FieldsWrapper>
       {fields.map((field, index) => {
