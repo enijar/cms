99e43368a91840d4c1d37e4f0d73495be45d00dc
Enijar <TheEnijar@gmail.com>
2023-05-13 20:01:31 +0100

    chore: update schemas

--- config/schemas.ts
+++ config/schemas.ts
@@ -1,49 +1,10 @@
 import { createSchema, fields } from "../client/src/cms/cms";
 
-export const home = createSchema({
-  title: fields.text(),
-  description: fields.richText(),
-  person: fields.group({
-    name: fields.text(),
-    bio: fields.richText(),
-    skinTone: fields.color(),
-    partner: fields.group({
-      name: fields.text(),
-      bio: fields.richText(),
-      friends: fields.list({
-        name: fields.text(),
-        info: fields.group({
-          dob: fields.datetime(),
-          height: fields.text(),
-        }),
-      }),
-    }),
-  }),
-  locations: fields.list({
-    address: fields.text(),
-    info: fields.richText(),
-  }),
-});
-
-export const about = createSchema({
-  title: fields.text(),
-  description: fields.richText(),
-});
-
 export const pages = createSchema({
   pages: fields.list(
     {
       name: fields.text(),
       title: fields.text(),
-    },
-    { titleField: "name" }
-  ),
-});
-
-export const test = createSchema({
-  items: fields.list(
-    {
-      name: fields.text(),
     },
     { titleField: "name" }
   ),
